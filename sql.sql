e7326ef17c6e2e45e55c5ed0cbfe3d59c9b30fbf
Cramwordplay <50309397+Cramwordplay@users.noreply.github.com>
2019-09-19 13:08:33 +0100

    Delete sql.sql

--- sql.sql
+++
@@ -1,9 +0,0 @@
-
-
-CREATE TABLE user (
-    id int(11) not null PRIMARY KEY AUTO_INCREMENT,
-    fname varchar(128) not null,
-    username varchar(128) not null,
-    email varchar(128) not null,
-    pwd varchar(1000) not null
-);
